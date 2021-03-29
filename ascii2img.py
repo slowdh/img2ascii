ab52e75144aed8ce7ef9f6d3e45bf47efac110b7
DonghyunKim <leok0909@snu.ac.kr>
2021-03-29 19:13:30 +0900

    variable video capture input shape support

--- ascii2img.py
+++ ascii2img.py
@@ -4,9 +4,9 @@ from PIL import Image, ImageOps, ImageDraw, ImageFont
 
 
 # image to numpy arr with appropriate size
-def preprocess_image(image, target_size):
+def preprocess_image(image, char_img_size):
     img = ImageOps.grayscale(Image.fromarray(image))
-    img = img.resize(target_size)
+    img = img.resize(char_img_size)
     np_img = np.array(img)
     return np_img
 
@@ -17,20 +17,16 @@ def convert(img):
     np_img = np.floor(np.array(img) / 255 * 10).astype('uint8')
     asc = []
 
-    # now, it's not so efficient.. but it works!
     for i in range(vertical):
         row_temp = ''.join([ascii_chars[np_img[i, j]] for j in range(horizontal)])
         asc.append(row_temp)
     asc_img = '\n'.join(asc)
 
-    ## faster? not that much...
-    # vectorized = np.vectorize(ascii_chars.__getitem__)(np_img)
-    # asc_img = '\n'.join([''.join(vectorized[i]) for i in range(len(vectorized))])
     return asc_img
 
-# text to image ((20, 10) px -> 1 char when window size is (600, 600), font_size = 16)
+# text to image ((19, 10) px -> 1 char when font_size = 16)
 def text_to_image(text, size):
-    img_size = (size[1] * 10, size[0] * 19)
+    img_size = (size[0], size[1])
     image_out = Image.new(mode='RGB', size=img_size, color=(0, 0, 0))
     draw = ImageDraw.Draw(image_out)
     font = ImageFont.truetype('fonts/DejaVuSansMono-Bold.ttf', 16)
@@ -38,22 +34,29 @@ def text_to_image(text, size):
     return image_out
 
 # real time webcam capture and convert!
-def capture_and_convert(size=(75, 25)):
-    cap = cv2.VideoCapture(0)
+def capture_and_convert(size=(640, 480), resize_factor=1., source_path=0, save_path=None):
+    cap = cv2.VideoCapture(source_path)
+    if size is not None:
+        size = (size[0] * resize_factor, size[1] * resize_factor)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
+    else:
+        size = (int(cap.get(3) * resize_factor), int(cap.get(4) * resize_factor))
+    char_img_size = (size[0] // 10, size[1] // 19)
 
-    while True:
+    while cap.isOpened():
         _, frame = cap.read()
-        gray = preprocess_image(frame, size)
+        gray = preprocess_image(frame, char_img_size)
         converted = convert(gray)
-        pil_img = text_to_image(converted, tuple(reversed(size)))
+        pil_img = text_to_image(converted, size)
         cv_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
         cv2.imshow("ASCII WORLD", cv_img)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    cao.release()
+    cap.release()
     cv2.destroyAllWindows()
 
 
 # test time!
-capture_and_convert()
+capture_and_convert(size=None, resize_factor=0.7, source_path=0, save_path=None)
